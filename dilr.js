5ee5e76dbfd57e50297ad25a55088b428a9ebf1b
Chisimdi Damian Ezeanieto <chisimdi@Chisimdi.local>
2018-11-17 23:43:01 +0100

    Updated.

---
+++ dilr.js
@@ -0,0 +1,60 @@
+{
+    countries: [{
+        name: 'Nigeria',
+        states: [{
+            name: '',
+            noOfLga: 50,
+            noOfWards: 5,
+            noOfPollingUnit: 20,
+            noOfInecRegVoters: 5000,
+            noOfFdVoters: 100000,
+            leaderId: 1,
+            lga: [{
+                name: '',
+                noOfWards: 20,
+                noOfPollingUnit: 20,
+                noOfInecRegVoters: 5000,
+                noOfFdVoters: 100000,
+                leaderId: 1,
+                wards:[{
+                    name: '',
+                    noOfPollingUnit: 20,
+                    noOfInecRegVoters: 5000,
+                    noOfFdVoters: 100000,
+                    leaderId: 1,
+                    pollingUnit:[{
+                        name: '',
+                        GIS: {},
+                        noOfInecRegVoters: 5000,
+                        noOfFdVoters: 100000,
+                        leaderId: 1,
+                        Voters:[{
+                            Personid
+                        }]
+                    }]
+                }]
+            }]
+        }]    
+    }];
+}
+
+{
+    person:[{
+        name: '',
+        dob: '',
+        address: '',
+        phonenumber: '',
+        occupation: '',
+        maritalStatus: '',
+        sex: '',
+        educationLevel: '',
+        volunteer: '',
+        status: '',
+        uplineId: '',
+        pvcCode: '',
+        candidate: '', 
+        officeId: '',
+        membershipNumber: '',
+        note: ''
+    }]
+}
